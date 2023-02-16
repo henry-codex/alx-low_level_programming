83a80552084e7b66d45f96951ea7defe58cb7c4c
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-02-16 09:23:45 -0800

    Doing C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-int main(void) 
-{
-	puts("\"Programming is like building a multilingual puzzle");
+
+
+ /**
+  *
+  * * main - Entry point
+  *
+  *  *
+  *
+  *   * Return: Always 0 (Success)
+  *
+  *    */
+
+ int main(void)
+
+ {
+
+		puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
