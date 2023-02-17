835e7bb47c41a43f28ad433aef464e6c0f366c54
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-02-17 13:23:38 -0800

    C programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int c, i;
+	int c, i;
 
-    for (c = '0'; c <= '9'; c++)
-    {
-        for (i = '0'; i <= '9'; i++)
-        {
-            if (c < i)
-            {
-                putchar(c);
-                putchar(i);
+	for (c = '0'; c <= '9'; c++)
+	{
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (c < i)
+		{
+		putchar(c);
+		putchar(i);
 
-                if (c != '8' || (c == '8' && i != '9'))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+		if (c != '8' || (c == '8' && i != '9'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
