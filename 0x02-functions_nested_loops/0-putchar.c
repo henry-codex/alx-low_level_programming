78be30767c3b21e008204dd096496c5f9f8c7629
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-02-21 12:21:14 -0800

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "_putchar\n";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (0);
+}
+
