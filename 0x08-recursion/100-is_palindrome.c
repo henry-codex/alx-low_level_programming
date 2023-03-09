8325faf096da4ffde7dfc3cee9cc8db563441473
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-03-08 21:33:05 -0800

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "string.h"
 /**
  * is_palindrome - find a palindrome string
  * @s: string for check
@@ -7,12 +8,18 @@
  */
 int is_palindrome(char *s)
 {
+	int len = strlen(s);
 	int i;
 
-	i = 0;
+	if (len <= 1)
+		return (1);
 
-	if (*s == i)
-		return (0);
+	for (i = 0; i < len / 2; i++)
+	{
+		if (*(s + i) != *(s + len - i - 1))
+			return (0);
+	}
+
 	return (1);
 }
 
