2f554c9fedb54b362818219d110d89b81d2ad9ac
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-03-08 22:00:32 -0800

    hmm

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,26 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: input string
+ * is_palindrome - check if a string is a palindrome
+ * @s: string to check
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	int len = 0, i;
+int len = strlen(s);
+int i;
 
-	/* Find the length of s */
-	while (*(s + len) != '\0')
-		len++;
+if (len <= 1)
+return (1);
 
-	/* Compare characters at opposite ends of s */
-	for (i = 0; i < len / 2; i++)
-	{
-		if (*(s + i) != *(s + len - 1 - i))
-			return (0);
-	}
+for (i = 0; i < len / 2; i++)
+{
+if (*(s + i) != *(s + len - i - 1)
+return (0);
+}
 
-	return (1);
+return (1);
 }
 
