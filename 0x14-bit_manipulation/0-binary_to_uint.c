8b1cbc554a7fb0dba8e1c3d79f6c4aed505bbe62
Henry Tetteh <httetteh@st.ug.edu.gh>
2023-04-09 09:42:01 -0700

    Doing git manipulations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0, binaryDigit = 0, index = 0;
+	unsigned int num = 0;
+	int len = 0;
 
-	if (!b)
+	if (b[len] == '\0')
 		return (0);
 
-	/* Get the size of the binary string */
-	while (b[index])
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		if (b[index] != '0' && b[index] != '1')
-			return (0);
-
-		index++;
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
 
-	/* Convert the binary string to a decimal number */
-	index--;
-	while (index >= 0)
-	{
-		binaryDigit = b[index] - '0';
-		decimal += binaryDigit << (len - index - 1);
-		index--;
-	}
-
-	return (decimal);
+	return (num);
 }
-
