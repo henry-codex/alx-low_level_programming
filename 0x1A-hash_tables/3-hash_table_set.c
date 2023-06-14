3f701bc82f4ed17a01b4d119c7d907892bfd8bcb
Henry-codex <httetteh@st.ug.edu.gh>
2023-06-14 15:58:49 -0700

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,34 +10,30 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	/* Check for invalid inputs */
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	/* Create a copy of the value */
-	char *value_copy = strdup(value);
+	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
-	/* Calculate the index to store the key-value pair */
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-	/* Traverse the linked list at the index */
-	hash_node_t *current = ht->array[index];
-	while (current != NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		/* Update value if key already exists in the linked list */
-		if (strcmp(current->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(current->value);
-			current->value = value_copy;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
-		current = current->next;
 	}
 
-	/* Create a new node and insert it at the beginning of the linked list */
-	hash_node_t *new = malloc(sizeof(hash_node_t));
+	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
 		free(value_copy);
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,47 +8,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	/* Check if the hash table is NULL */
-	if (ht == NULL)
-		return;
-
-	/* Initialize variables */
 	hash_node_t *node;
 	unsigned long int i;
 	unsigned char comma_flag = 0;
 
-	/* Print the opening brace of the hash table */
-	printf("{");
+	if (ht == NULL)
+		return;
 
-	/* Iterate over each index of the hash table */
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		/* Check if the current index is not empty */
 		if (ht->array[i] != NULL)
 		{
-			/* Add a comma if necessary */
 			if (comma_flag == 1)
 				printf(", ");
 
-			/* Traverse the linked list at the current index */
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				/* Print the key-value pair */
 				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
-
-				/* Add a comma if there are more nodes */
 				if (node != NULL)
 					printf(", ");
 			}
-
-			/* Set the comma flag to 1 */
 			comma_flag = 1;
 		}
 	}
-
-	/* Print the closing brace of the hash table */
 	printf("}\n");
 }
-
